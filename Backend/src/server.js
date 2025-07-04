691e2344233f7278d5782bb0bc53af95b8ef672d
Hayou <hayoulemessa96@gmail.com>
2025-07-04 23:06:53 +0300

    no comment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -5,9 +5,17 @@ import { connectDB } from "./config/db.js";
 const app =  express();
 
 
-connectDB();
-
 app.get('/', (req, res) => res.send("Hello from Server"))
 
-app.listen(ENV.PORT, () => console.log(`Server is running on PORT: ${ENV.PORT}`));
+const startServer = async() => {
+    try {
+        await connectDB();
+        
+        app.listen(ENV.PORT, () => console.log("Server is up and running on PORT: ", ENV.PORT));
+    } catch (error) {
+        console.error("Failed to start the server: ", error.message);
+        process.exit(1);
+    }
+};
 
+startServer();
